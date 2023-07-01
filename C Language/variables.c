58e7ea930ddda9c27ea33ea457323bbd4cb4ba8d
Raman Biju <ramanbijuomkar@gmail.com>
2023-07-01 19:05:09 +0530

    lecture 02 updated

---
+++ C Language/variables.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// for string we use ""
+// for characters we use ''
+
+/*
+QUESTION :  CREATE A VARIABLE A NAD B
+            ASSIGN VALUES TO THEM
+            PRINT THEIR SUM, DIFFERNCE, PRODUCT AND QUOTIENT
+*/
+
+//  sum = +, diff = -, pro= *, quot = /
+
+void main(){
+    // types of variables (int (integer) 4 bytes, float (float) 4 bytes, char (characters) 1 bytes)
+
+    // int a;  // Declaring
+    // a = 1;  // assigning
+    // int a = 1001;
+    // a = 1;
+    // printf("%i", a);
+
+    // float f;
+    // f = 2.0;
+    // float f = 3.0;
+    // double d = 2.0;
+
+    // char c;
+    // c = 'R';
+    // char c = 'R';
+
+    // printf("I love C");
+
+    // FORMAT SPECIFIERS (%)
+    /*
+    Integer (int): %d or %i
+    Float (float) or Double (double): %f
+    Character (char): %c
+    String : %s
+    */
+    // printf("%i", a);
+    // printf("%d", 1);
+
+    // printf("%d bytes allocated in the memory", sizeof(d));
+    
+    // int a = 5;
+    // int b = 2;
+    // int c = a + b;
+    // 1 + 2 = 3
+    // printf("%d + %d = %d", a, b, c);
+
+    // int a = 5, b = 2;
+}

be3d7c2df707b1c8178163a0494ebe82ec026382
Raman Biju <ramanbijuomkar@gmail.com>
2023-07-01 12:26:06 +0530

    INITIAL COMMIT

---
+++ C Language/output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* multi-line comment
+this is a c program
+this is a program ot print the output
+the program is used to ...
+*/
+
+void main(){
+    // this is a print function that prints a literal (single line comment)
+    printf("89\n56");   // it prints 86 and 56
+}
---
+++ C Language/test.c
@@ -0,0 +1,8 @@
+// header file
+#include<stdio.h>
+
+// here is the main function
+void main(){
+    // function to print our message
+    printf("Hello world");
+}
